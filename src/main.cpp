df0464d8a64d5dbf9197ec48c8e0ef23793af811
Len Bucchino <len@bitfly.org>
2018-11-04 17:40:14 -0600

    testing ranges

--- src/main.cpp
+++ src/main.cpp
@@ -1,59 +1,75 @@
 #include <Arduino.h>
-#include <stdarg.h>
-#include <PID_v1.h>
-#include "Servo_Control/Servo_Control.h"
+#include <Adafruit_PWMServoDriver.h>
 
-#define MICROS_PER_SECOND 1000000
+#define SERVOMIN 220    //150 // this is the 'minimum' pulse length count (out of 4096)
+#define SERVOMAX 560    //600 // this is the 'maximum' pulse length count (out of 4096)
+#define SERVOCENTER 382 //375 //((SERVOMAX - SERVOMIN) / 2) + SERVOMIN
 
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
+// called this way, it uses the default address 0x40
+Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
+// you can also call it with a different address you want
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x41);
+// you can also call it with a different address and I2C interface
+//Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(&Wire, 0x40);
 
-short degreesOfTravel = 165;
-short degreesPerSecondMax = 600;
-short degreesPerSecondLimit = 22;
-short elapsedTimeMicros = 0;
-short lastTime = -1;
 
-unsigned long last = micros();
-unsigned long cur = last;
+//pca8685 OE pin - low all outputs enabled, board pulls it low by default
 
-//Define Variables we'll be connecting to
-double Setpoint, Input, Output;
-
-//Define the aggressive and conservative Tuning Parameters
-double aggKp = 4, aggKi = 0.2, aggKd = 1;
-double consKp = 1, consKi = 0.05, consKd = 0.25;
-
-//Specify the links and initial tuning parameters
-PID myPID(&Input, &Output, &Setpoint, consKp, consKi, consKd, DIRECT);
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.println("Spiderbot starting");
+  Serial.println("Spiderbot testing");
 
-  myPID.SetMode(AUTOMATIC);
+  //D2 wired to pca8685 OE
+  pinMode(PIND2, OUTPUT);
+
+  //disable
+  digitalWrite(PIND2, HIGH);
+
+  pwm.begin();
+  pwm.setPWMFreq(60); // Analog servos run at ~60 Hz updates
+  delay(10);
+  delay(5000);
+  Serial.println("Start");
+  digitalWrite(PIND2, LOW);
+  pwm.setPWM(0, 0, SERVOMIN);
+  delay(10);
 }
 
 void loop()
 {
-  // cur = micros();
-  // Serial.println(cur - last);
-  // delay(1000);
-  // last = cur;
+    //pwm.setPWM(0, 0, SERVOMIN);
+    //delay(100);
+  //Drive each servo one at a time
 
-  myPID.Compute();
+  Serial.println("min");
+  delay(1000);
 
-  Serial.println(Output);
-  delay(500);
-}
+  for (uint16_t pulselen = SERVOMIN; pulselen < SERVOMAX; pulselen++)
+  {
+    pwm.setPWM(0, 0, pulselen);
 
-void p(char *fmt, ...)
-{
-  char buf[128]; // resulting string limited to 128 chars
-  va_list args;
-  va_start(args, fmt);
-  vsnprintf(buf, 128, fmt, args);
-  va_end(args);
-  Serial.print(buf);
+    if (pulselen == SERVOCENTER)
+    {
+      delay(1000);
+    }
+
+    delay(1);
+  }
+
+  Serial.println("max");
+  delay(1000);
+
+  for (uint16_t pulselen = SERVOMAX; pulselen > SERVOMIN; pulselen--)
+  {
+    pwm.setPWM(0, 0, pulselen);
+
+    if (pulselen == SERVOCENTER)
+    {
+      delay(1000);
+    }
+
+    delay(1);
+  }
 }
